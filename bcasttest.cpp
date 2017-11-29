2beca86d301577969521b3e90cfef52242e09039
CHLLHC <chl41993@gmail.com>
2017-11-28 16:49:20 -0800

    bc2

--- bcasttest.cpp
+++ bcasttest.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	}
 
 	uint64_t test = n;
-	for (uint64_t i = 0;i<proc0_size;++i){
+	for (uint64_t i = 0;i<9592;++i){
 		MPI_Bcast(&test, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
 	}
 
